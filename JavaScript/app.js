13e9608c6c880357186053aafea75c4eac27158b
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-07-22 14:42:03 -0400

    Estilización de mensajes de error

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,7 @@
 import { valida } from "./validacion";
+
 const inputs = document.querySelectorAll("input");
+
 inputs.forEach(input => {
     input.addEventListener("blur", (input) => {
         valida(input.target);
--- JavaScript/validacion.js
+++ JavaScript/validacion.js
@@ -1,12 +1,56 @@
 export function valida(input){
 	const tipoDeInput = input.dataset.tipo;
+
 	if(validadores[tipoDeInput]){
 		validadores[tipoDeInput](input);
+	}
+
+	if(input.validity.valid){
+		input.parentElement.classList.remove("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML = "";
+	}else{
+		input.parentElement.classList.add("input-container--invalid");
+		input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+	}
+}
+
+const tipoDeErrores = [
+	"valueMissing",
+	"typeMismatch",
+	"patternMismatch",
+	"customError"
+]
+
+const mensajesDeError = {
+	nombre: {
+		valueMissing: "El campo nombre no puede estar vacio"
+	},
+	email: {
+		valueMissing: "El campo correo no puede estar vacio",
+		typeMismatch: "El correo no es valido"
+	},
+	password: {
+		valueMissing: "El campo contraseña no puede estar vacio",
+		patternMismatch: "Debe contener al menos 8 caracteres, al menos una letra minúscula, al menos una letra mayúscula y al menos un caracter especial"
+	},
+	nacimiento: {
+		valueMissing: "El campo fecha de nacimiento no puede estar vacio",
+		customError: "Debes tener al menos 18 años de edad"
 	}
 }
 
 const validadores = {
 	nacimiento: (input) => validarNacimiento(input),
+}
+
+function mostrarMensajeDeError(tipoDeInput, input){
+	let mensaje = "";
+	tipoDeErrores.forEach((error) => {
+		if(input.validity[error]){
+			mensaje = mensajesDeError[tipoDeInput][error];
+		}
+	});
+	return mensaje;
 }
 
 function validarNacimiento(input){
