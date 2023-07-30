cb8288d0d9f3a11e8f5eb801c33e91e84ee06bb1
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-07-30 05:26:50 -0400

    Orden de ejecución, FETCH API, refactorizar el código

--- services/client-service.js
+++ services/client-service.js
@@ -1,34 +1,14 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido = `
-        <td class="td" data-td>${nombre}</td>
-        <td>${email}</td>
-        <td>
-        <ul class="table__button-control">
-            <li>
-            <a
-                href="../screens/editar_cliente.html"
-                class="simple-button simple-button--edit"
-                >Editar</a
-            >
-            </li>
-            <li>
-            <button
-                class="simple-button simple-button--delete"
-                type="button"
-            >
-                Eliminar
-            </button>
-            </li>
-        </ul>
-        </td>
-    `;
-    linea.innerHTML = contenido;
-    return linea;
+const listaClientes = () => {
+    return fetch("http://localhost:3000/perfil").then(respuesta => {
+        return respuesta.json();
+    });
 }
 
-const table = document.querySelector("[data-table]");
+export const clientServices = {
+    listaClientes
+}
 
+/*
 const listaClientes = () => {
     const promise = new Promise((resolve, reject) => {
         const http = new XMLHttpRequest();
@@ -44,11 +24,4 @@ const listaClientes = () => {
         }
     });
     return promise;
-}
-
-listaClientes().then((data) => {
-    data.forEach((perfil) => {
-        const nuevaLista = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLista);
-    });
-}).catch((error) => alert("Ocurrió un error"));
+}*/
