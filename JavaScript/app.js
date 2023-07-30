4a34776b09a4c1c53fa0623c1743872f2bee0a46
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-07-30 02:42:31 -0400

    Rama vacia

--- JavaScript/app.js
+++
@@ -1,9 +0,0 @@
-import { valida } from "./validacion";
-
-const inputs = document.querySelectorAll("input");
-
-inputs.forEach(input => {
-    input.addEventListener("blur", (input) => {
-        valida(input.target);
-    });
-});
